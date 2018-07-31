bd4d954840c38840a835da9ae4fcf6f5aae7b9dd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-31 23:54:52 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,15 +21,16 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
+  end
+  
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
   
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
 
   post '/pets/:id' do 
     @pet = Pet.find(params[:id])
